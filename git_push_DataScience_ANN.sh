b1d32eeb6fd93d99f38d3983c589acc0b2babd15
島大樹 <shimadaiki@shimadaikinoMacBook-Pro.local>
2022-01-20 12:05:57 +0900

    1

--- git_push_DataScience_ANN.sh
+++ git_push_DataScience_ANN.sh
@@ -3,5 +3,5 @@ read str
 git add .
 git commit -m ${str}
 git branch -M main
-git remote add origin https://github.com/shimakaru/DataScience_ANN.git
+git remote add origin https://github.com/shimakaru/DataScience.git
 git push -u origin main
